9e22e886ad917b596b78e31d15a4fd172122b3e8
debojyotisen <deb.m.sen@gmail.com>
2023-04-25 21:26:57 +0530

    V1

--- src/main/java/com/sendebojyoti/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/sendebojyoti/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -8,29 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
-	@Autowired
-	private InstanceInformationService service;
 
-	@GetMapping(path = "/")
-	public String imUpAndRunning() {
-		return "{healthy:true}";
-	}
+    @Autowired
+    private InstanceInformationService service;
 
-	
-	@GetMapping(path = "/hello-world")
-	public String helloWorld() {
-		return "Hello World " + " V1 " + service.retrieveInstanceInfo();
-	}
+    @GetMapping(path = "/")
+    public String imUpAndRunning() {
+        return "{healthy:true}";
+    }
 
-	@GetMapping(path = "/hello-world-bean")
-	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World");
-	}
 
-	/// hello-world/path-variable/in28minutes
-	@GetMapping(path = "/hello-world/path-variable/{name}")
-	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-		return new HelloWorldBean(String.format("Hello World, %s", name));
-	}
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World " + " V1 " + service.retrieveInstanceInfo();
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
 }
